031f46e8ed024f4fbadc08f91ee9fb9a3132cb69
HagarOthman2002 <hagarosman355@gmail.com>
2025-04-17 15:52:42 +0200

    removing unneccessary imports

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,21 +1,16 @@
 import Header from "./Components/Header/Header.jsx";
-import CoreConcept from "./Components/CoreConcept";
-import { CORE_CONCEPTS, EXAMPLES } from "./Data.js";
-import TapButton from "./Components/TabButton.jsx";
-import { Component, useState } from "react";
-import CoreConcepts from "./Components/CoreConcepts.jsx"
+import CoreConcepts from "./Components/CoreConcepts.jsx";
 import Examples from "./Components/Examples.jsx";
 
 function App() {
-  
   return (
     <div>
       <Header />
 
       <main>
-        <CoreConcepts/>
-        
-        <Examples/>
+        <CoreConcepts />
+
+        <Examples />
       </main>
     </div>
   );
